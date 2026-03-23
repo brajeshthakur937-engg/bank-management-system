69bd09def4e7a786115ac6aba3bbac3af04e9fee
Brajesh Thakur <brajeshthakur937@gmail.com>
2026-03-23 10:12:58 +0530

    Create User entity with fields and annotations

---
+++ User.java
@@ -0,0 +1,10 @@
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+}
